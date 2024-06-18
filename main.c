a18f00bdfa71afcf2b0d9126d708ef7847856def
anhphan <anh.phan156@protonmail.com>
2024-06-17 22:40:20 -0400

    hello world

--- main.c
+++ main.c
@@ -2,26 +2,33 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-#define BLINK_MS 5000
-#define PRESCALER 10
-#define CLOCK_RATE 15998976
-#define COUNTER_VALUE (CLOCK_RATE >> PRESCALER)
+#define mmio(x) ((volatile void*)x)
 
-ISR(TIMER1_COMPA_vect){
-	PORTB ^= _BV(PORTB5);
+ISR (TIMER1_OVF_vect){
+	volatile unsigned char *portd = mmio(0x2B);
+	volatile unsigned short *tcnt1 = mmio(0x84);
+	*portd ^= 1 << PORTD2;
+
+	*tcnt1 = 65535 - (F_CPU/1024) / 2;
 }
 
 int main() {
-  /*DDRB |= 0b00100000;*/
-  DDRB |= _BV(PORTB5);
 
-	OCR1A = COUNTER_VALUE;
+	volatile unsigned char *ddrd = mmio(0x2A);
+	volatile unsigned short *tcnt1 = mmio(0x84);
+	volatile unsigned char *tccr1a = mmio(0x80);
+	volatile unsigned char *tccr1b = mmio(0x81);
+	volatile unsigned char *timsk1 = mmio(0x6F);
 
-	TCCR1A = 0x00;
-	TCCR1B |= (1 << WGM12);
-	TCCR1B |= (1 << CS10) | (1 << CS12);
+	*ddrd |= 1 << DDD2;
 
-	TIMSK1 |= (1 << OCIE1A);
+	*tcnt1 = 65535 - (F_CPU/1024) / 2;
+
+	*tccr1a = 0x00;
+	// 16MHz/1024 = 15625Hz
+	*tccr1b |= (1 << CS10) | (1 << CS12);
+
+	*timsk1 |= (1 << TOIE1);
 
 	sei();
 
